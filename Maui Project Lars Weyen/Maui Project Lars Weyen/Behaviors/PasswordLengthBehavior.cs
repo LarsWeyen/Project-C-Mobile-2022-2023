ac3b140343abf4d6b7e56d3a1ffde451a8b4c13f
LarsWeyen <weyen.lars@gmail.com>
2023-01-05 19:44:58 +0100

    Added a CharLimitConverter and a PasswordLengthBehavior

---
+++ Maui Project Lars Weyen/Maui Project Lars Weyen/Behaviors/PasswordLengthBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui_Project_Lars_Weyen.Behaviors
+{
+    class PasswordLengthBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            string input = args.NewTextValue;
+
+            ((Entry)sender).TextColor = input.Length >= 6 ? Colors.Green: Colors.Red;
+        }
+    }
+}
---
+++ Maui Project Lars Weyen/Maui Project Lars Weyen/Converters/CharLimitConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui_Project_Lars_Weyen.Converters
+{
+    class CharLimitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string beschrijving = (string)value;
+            if (beschrijving.Length <= 24)
+            {
+                return beschrijving;
+            }
+            return beschrijving.Substring(24) + " ...";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
